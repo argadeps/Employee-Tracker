421209c9df289e69073396d9cc62c961ca318bae
[Pral Argade] <[pralargade@outlook.com]>
2025-03-01 15:59:58 -0500

    update seed

--- Main/src/db/seed.sql
+++ Main/src/db/seed.sql
@@ -4,7 +4,7 @@
 INSERT INTO department
     (name)
 VALUES
-    ('Sales'),
+    ('Business Development'),
     ('Engineering'),
     ('Finance'),
     ('Legal');
@@ -12,23 +12,23 @@ VALUES
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Sales Lead', 100000, 1),
-    ('Salesperson', 80000, 1),
+    ('Lead Business Developer', 100000, 1),
+    ('Business Developer', 80000, 1),
     ('Lead Engineer', 150000, 2),
     ('Software Engineer', 120000, 2),
     ('Account Manager', 160000, 3),
     ('Accountant', 125000, 3),
-    ('Legal Team Lead', 250000, 4),
-    ('Lawyer', 190000, 4);
+    ('Senior Attorney', 250000, 4),
+    ('Attorney', 190000, 4);
 
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, NULL),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
-    ('Tom', 'Allen', 8, 7);
+    ('Thomas', 'Jansen', 1, NULL),
+    ('Peter', 'Andrew', 2, 1),
+    ('Ashley', 'Johnson', 3, NULL),
+    ('Kevin', 'Murphy', 4, 3),
+    ('Catherine', 'Kensington', 5, NULL),
+    ('John', 'Carter', 6, 5),
+    ('Elizabeth', 'Jameson', 7, NULL),
+    ('Mark', 'Greene', 8, 7);
